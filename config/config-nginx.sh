8b8132c48695e8ebe57abd51cbed3fb4998666e5
gene croy <genercroy@gmail.com>
2022-04-10 16:09:18 -0500

    nginx config

--- config/config-nginx.sh
+++ config/config-nginx.sh
@@ -1,2 +1,2 @@
-ln -s /root/mealie/config/ssl.conf /etc/nginx/sites-available/ssl.conf
-ln -s /root/mealie/config/ssl-redirect.conf /etc/nginx/sites-available/ssl-redirect.conf
+ln -s /root/mealie/config/ssl.conf /etc/nginx/sites-enabled/ssl.conf
+ln -s /root/mealie/config/ssl-redirect.conf /etc/nginx/sites-enabled/ssl-redirect.conf
